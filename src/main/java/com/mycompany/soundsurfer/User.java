2f0a51317d446578e08aed528299fcd5bf6149a8
deep-patel21 <deepstudios21@gmail.com>
2024-01-31 13:14:21 -0500

    added getters and setters in User.java

--- src/main/java/com/mycompany/soundsurfer/User.java
+++ src/main/java/com/mycompany/soundsurfer/User.java
@@ -7,6 +7,11 @@ package com.mycompany.soundsurfer;
 public class User{
     private String username;
     private String password; 
+    private int numSongs;
+    private int numArtists;
+    private String favGenre;
+    private String favArtist;
+    private String favSong;
     
     public User(String user, String pass){
         username = user; 
@@ -28,4 +33,51 @@ public class User{
     public void setPassword(String pass){   
         password = pass; 
     }
+    
+    public int getNumSongs() {
+        return this.numSongs;
+    }
+    
+    public void setNumSongs(int numSongs) {
+        this.numSongs = numSongs;
+    }
+    
+    public int getNumArtists() {
+        return this.numArtists;
+    }
+   
+    public void setNumArtists (int numArtists) {
+        this.numArtists = numArtists;
+    }
+    
+    public String getFavGenre() {
+        //later when array of genres is passed, a count of each can be recorded 
+        //returning the highest one
+        return this.favGenre;
+    }
+   
+    public void setFavGenre (String favGenre) {
+        this.favGenre = favGenre;
+    }
+    
+    public String getFavArtist() {
+        //later when array of artists is passed, a count of each can be recorded
+        //returning the highest one
+        return this.favArtist;
+    }
+   
+    public void setFavArtist (String favArtist) {
+        this.favArtist = favArtist;
+    }
+    
+    public String getFavSong() {
+        return this.favSong;
+    }
+    
+    public void setFavSong(String favSong) {
+        //later when array of songs is passed, a count of each can be recorded
+        //returning the highest one
+        this.favSong = favSong;
+    }
 }
+ 
